e24ff70802191f7724ca84caca0d8d011f166bb8
geimaj <16000925@vcconnect.co.za>
2018-10-13 19:13:51 +0200

    add map fragment

--- app/src/main/java/com/example/jamie/spam/MainActivity.java
+++ app/src/main/java/com/example/jamie/spam/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.jamie.spam;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "DEBUGZ";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        if(isServicesVersionCorrect()){
+//            init();
+//        }
+
+    }
+
+    public boolean isServicesVersionCorrect() {
+        Log.d(TAG, "checking google services version");
+
+        int avaiable = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (avaiable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(avaiable)) {
+            Log.d(TAG, "resolveable VERSION ERROR");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, avaiable, ERROR_DIALOG_REQUEST);
+        } else {
+            Log.d(TAG, "API VERSION ERROR");
+        }
+            return false;
+    }
+
+    private void init(){
+
     }
 }
