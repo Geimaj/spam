ec2a84473fb111a8b3b5901c9084a2b95674e938
geimaj <16000925@vcconnect.co.za>
2018-10-14 18:15:42 +0200

    get destination from place picker

--- app/src/main/java/com/example/jamie/spam/MainActivity.java
+++ app/src/main/java/com/example/jamie/spam/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.jamie.spam;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,27 +18,61 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.firebase.FirebaseApp;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private static final String TAG = "DEBUGZ";
 
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final int RC_SIGN_IN = 1;
+    private static final int RC_PLACE_PICKER_DESTINATION = 2;
+    private static final int RC_PLACE_PICKER_ORIGIN = 3;
+
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private GeoDataClient mGeoDataClient;
+    private boolean mLocationPermissionGranted = false;
+
+    private Place from;
+    private Place destination;
+
+    private EditText etDestination;
+    private EditText etFrom;
+    private MapFragment mapFragment;
+
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        getLocationPermission();
 
         //setup auth
 
@@ -46,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user != null){
+                if (user != null) {
                     //signed in
                     onSignedInInitialized();
                 } else {
@@ -73,45 +110,155 @@ public class MainActivity extends AppCompatActivity {
         };
 
         //diable edit texts
-        final EditText etFrom = (EditText) findViewById(R.id.etFrom);
-        EditText etDestination = (EditText) findViewById(R.id.etDestination);
+        etFrom = (EditText) findViewById(R.id.etFrom);
+        etDestination = (EditText) findViewById(R.id.etDestination);
+
         etDestination.setInputType(InputType.TYPE_NULL);
         etFrom.setInputType(InputType.TYPE_NULL);
 
-
-        etFrom.setOnTouchListener(new View.OnTouchListener() {
+        etFrom.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                return false;
+            public void onClick(View view) {
+                pickPlace(RC_PLACE_PICKER_ORIGIN);
             }
         });
 
-        etDestination.setOnTouchListener(new View.OnTouchListener() {
+        etFrom.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                return false;
+            public void onFocusChange(View view, boolean b) {
+                pickPlace(RC_PLACE_PICKER_ORIGIN);
+            }
+        });
+
+        etDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickPlace(RC_PLACE_PICKER_DESTINATION);
+            }
+        });
+
+        etDestination.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                pickPlace(RC_PLACE_PICKER_DESTINATION);
+            }
+        });
+
+        FloatingActionButton fabDirections = (FloatingActionButton) findViewById(R.id.fabGetDirections);
+
+        fabDirections.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (destination == null) {
+                    Toast.makeText(MainActivity.this, "Select a destination", Toast.LENGTH_LONG).show();
+                }
+
+                if (mLocationPermissionGranted) {
+                    //next activity
+                } else {
+                    getLocationPermission();
+                }
+
+            }
+        });
+
+        FloatingActionButton fabLocation = (FloatingActionButton) findViewById(R.id.fabLocation);
+
+        fabLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mLocationPermissionGranted){
+                    Log.d(TAG, "GOT PERMS");
+                } else {
+                    getLocationPermission();
+                }
             }
         });
 
 
-
-        if(isServicesVersionCorrect()){
+        if (isServicesVersionCorrect()) {
             init();
         }
 
 
     }
 
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+    }
+
+    private void getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    private void pickPlace(int PLACE_PICKER_REQUEST) {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //check if the result is coming from the sign in
-        if(requestCode == RC_SIGN_IN){
-            if(resultCode == RESULT_OK){
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
                 Toast.makeText(MainActivity.this, "Signed in", Toast.LENGTH_LONG).show();
-            } else if (resultCode == RESULT_CANCELED){
+            } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(MainActivity.this, "Signed in canceled", Toast.LENGTH_LONG).show();
                 finish();
+            }
+        } else if (requestCode == RC_PLACE_PICKER_DESTINATION) {
+            if(resultCode == RESULT_OK){
+                Place place = PlacePicker.getPlace(data, this);
+                destination = place;
+                etDestination.setText(place.getAddress());
+
+                MarkerOptions destinationMarker
+                        = new MarkerOptions()
+                            .position(destination.getLatLng())
+                            .title(destination.getName().toString());
+
+                map.addMarker(destinationMarker);
+
+
+            } else if(resultCode == RESULT_CANCELED){
+                Log.d(TAG, "Place pick canceled");
             }
         }
     }
@@ -120,7 +267,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(mAuthStateListener != null){
+        if (mAuthStateListener != null) {
             mAuth.addAuthStateListener(mAuthStateListener);
         }
     }
@@ -128,7 +275,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        if(mAuthStateListener != null){
+        if (mAuthStateListener != null) {
             mAuth.removeAuthStateListener(mAuthStateListener);
         }
     }
@@ -155,10 +302,15 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Log.d(TAG, "API VERSION ERROR");
         }
-            return false;
+        return false;
     }
 
-    private void init(){
+    private void init() {
 
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
     }
 }
